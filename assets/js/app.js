a8dd6e00d4b3fa6b571f4ebb6a94a00755b455a9
GreeceArtemis <greece.rapu@gmail.com>
2017-04-18 11:59:36 -0500

    cambios js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,55 +1,49 @@
 var pandas=document.getElementsByClassName("imgPandas");
 var columna=document.getElementsByClassName("col");
-var p1=document.getElementById("p1");
-var p2=document.getElementById("p2");
-var p3=document.getElementById("p3");
-var p4=document.getElementById("p4");
-console.log(columna);
+var btnClose=document.getElementsByClassName("btnClose");
 var restaurar=document.getElementById("restaurar");
 var extincion=document.getElementById("extincion");
 var origen=document.getElementById("origen");
-console.log(panda1);
-console.log(panda1.value);
-p1.addEventListener('click',function(e) {
+
+//Closing Pandas
+btnClose[0].addEventListener('click',function(e) {
       e.preventDefault();
   pandas[0].style.display="none";
 });
-p2.addEventListener('click',function(e) {
+btnClose[1].addEventListener('click',function(e) {
       e.preventDefault();
   pandas[1].style.display="none";
 });
-p3.addEventListener('click',function(e) {
+btnClose[2].addEventListener('click',function(e) {
       e.preventDefault();
   pandas[2].style.display="none";
 });
-p4.addEventListener('click',function(e) {
+btnClose[3].addEventListener('click',function(e) {
       e.preventDefault();
   pandas[3].style.display="none";
 });
-extincion.addEventListener('click',function(e) {
-  e.preventDefault();
-  if(columna[1].style.visibility=="hidden") {
-    columna[1].style.visibility="visible"
-  }
-  else{
-    columna[1].style.visibility="hidden";
-  }
-
-});
+//
 origen.addEventListener('click',function(e) {
       e.preventDefault();
       if(columna[0].style.visibility=="hidden") {
         columna[0].style.visibility="visible"
-      }
+        }
       else{
         columna[0].style.visibility="hidden";
-      }
+        }
 });
-
+extincion.addEventListener('click',function(e) {
+    e.preventDefault();
+    if(columna[1].style.visibility=="hidden") {
+        columna[1].style.visibility="visible"
+      }
+    else{
+        columna[1].style.visibility="hidden";
+        }
+});
 restaurar.addEventListener('click',function(e) {
-      e.preventDefault();
-  pandas[0].style.display="inline-block";
-  pandas[1].style.display="inline-block";
-  pandas[2].style.display="inline-block";
-  pandas[3].style.display="inline-block";
+    e.preventDefault();
+    for(var i=0;i<pandas.length;i++){
+        pandas[i].style.display="inline-block";
+      }
 });
